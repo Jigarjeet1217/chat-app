54db8a7bfe100482f55af50807b5dd0845bf6cd4
Jigarjeet Singh <52489280+Jigarjeet1217@users.noreply.github.com>
2022-06-03 14:29:02 +0530

    added links to created rooms

--- src/components/rooms/ChatRoomList.js
+++ src/components/rooms/ChatRoomList.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import { Nav } from 'rsuite';
+import { Link, useLocation } from 'react-router-dom';
+import { Loader, Nav } from 'rsuite';
+import { useRooms } from '../../context/rooms.context';
 import RoomItems from './RoomItems';
 
 const ChatRoomList = ({ aboveHeight }) => {
+  const rooms = useRooms();
+  const location = useLocation();
   return (
     <Nav
       appearance="subtle"
@@ -12,10 +16,25 @@ const ChatRoomList = ({ aboveHeight }) => {
       style={{
         height: `calc(100vh - ${aboveHeight + 30}px)`,
       }}
+      activeKey={location.pathname}
     >
-      <Nav.Item>
-        <RoomItems />
-      </Nav.Item>
+      {!rooms && (
+        <Loader center vertical speed="slow" content="Loading" size="md" />
+      )}
+      {rooms &&
+        rooms.length &&
+        rooms.map(room => {
+          return (
+            <Nav.Item
+              id={room.id}
+              componentClass={Link}
+              to={`/chat/${room.id}`}
+              eventKey={`/chat/${room.id}`}
+            >
+              <RoomItems room={room} />
+            </Nav.Item>
+          );
+        })}
     </Nav>
   );
 };
